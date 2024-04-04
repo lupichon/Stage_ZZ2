33d6e21dc7d20541d702d059ae0c0303769a295b
lupichon <lucaspichon444@gmail.com>
2024-04-04 12:58:09 +0200

    Ajout du module window + bouton + ecriture

--- main.py
+++ main.py
@@ -1,41 +1,75 @@
-import modules
+import wiiboard
+import window
 import pygame
 import time
-import matplotlib
 
 def main():
-	board = modules.Wiiboard()
+	board = wiiboard.Wiiboard()
 	pygame.init()
 
-	graph = modules.graph(1000,1000,"Center of Gravity")
+	graph = window.graph(1000,1000,"Center of Gravity")
 	
 	address = board.discover()
 	board.connect(address) 
 
 	running = True
+	while (running):
+		graph.display_menu_screen()
+		for event in pygame.event.get() : 
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if graph.button_rect.collidepoint(pygame.mouse.get_pos()):
+					running = False
+
+			elif event.type == pygame.QUIT:
+				running = False
+
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RETURN:  
+					try:
+						number = int(graph.input_text)  
+						print("Nombre entré :", number)
+					except ValueError:
+						print("Erreur : entrée invalide")
+				elif event.key == pygame.K_BACKSPACE:  
+					graph.input_text = graph.input_text[:-1] 
+				else:
+					graph.input_text += event.unicode
+
+			elif event.type == wiiboard.WIIBOARD_CONNECTED:
+				board.setLight(True)
+
+			elif event.type == wiiboard.WIIBOARD_DISCONNECTED:
+				board.setLight(False)
+
+
+		# Rafraîchir l'affichage
+		pygame.display.flip()
+
+	
+	running = True
 
 	while(running and board.status == "Connected"):
 		for event in pygame.event.get():
-			if event.type == modules.WIIBOARD_MASS:
-				#print ("Total weight: " + str(event.mass.totalWeight) + ". Top left: " + str(event.mass.topLeft) + ". Bottom left: " + str(event.mass.bottomLeft) + ". Top right: " + str(event.mass.topRight) + ". Bottom right: " + str(event.mass.bottomRight))
-				#print("x : " + str(event.mass.CoMx) + " y : " + str(event.mass.CoMy))
-				graph.display(event)
+			if event.type == wiiboard.WIIBOARD_MASS:
+				graph.display_main_screen(event.mass)
 			
-			elif event.type == modules.WIIBOARD_BUTTON_PRESS:
+			elif event.type == wiiboard.WIIBOARD_BUTTON_PRESS:
 				print("Button pressed!")
 
-			elif event.type == modules.WIIBOARD_BUTTON_RELEASE:
+			elif event.type == wiiboard.WIIBOARD_BUTTON_RELEASE:
 				print("Button released")
 			
 			elif event.type == pygame.QUIT:
 				running = False
 
-			elif event.type == modules.WIIBOARD_CONNECTED:
+			elif event.type == wiiboard.WIIBOARD_CONNECTED:
 				board.setLight(True)
 
-			elif event.type == modules.WIIBOARD_DISCONNECTED:
+			elif event.type == wiiboard.WIIBOARD_DISCONNECTED:
 				board.setLight(False)
 			
+		pygame.display.flip()
+
 	board.disconnect()
 	pygame.quit()
 	print("end")
