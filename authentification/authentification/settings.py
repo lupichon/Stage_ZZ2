e7ec4de4d415e0b94e6183a2a3375d0a8d19484f
lupichon <lucaspichon444@gmail.com>
2024-04-09 15:49:08 +0200

    Premier affichage du centre de gravité sur la page web

--- authentification/authentification/settings.py
+++ authentification/authentification/settings.py
@@ -16,6 +16,7 @@ from . info import *
 import django
 from django.utils.encoding import force_str
 django.utils.encoding.force_text = force_str
+import os
 
 EMAIL_USE_TLS = EMAIL_USE_TLS
 EMAIL_HOST = EMAIL_HOST
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
+    "WBB",
 ]
 
 MIDDLEWARE = [
@@ -128,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
